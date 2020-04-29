a90264fd2d9d9bb1acf8dd43d0bd63ce616f58f6
InflexCZE <martin.pavlicek11@gmail.com>
2020-04-29 22:21:51 +0200

    [Hotfix] Ignore PatchZone Dlls in mod folder

--- PatchZone/Mods/DataSources.cs
+++ PatchZone/Mods/DataSources.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using ModestTree;
 using PatchZone.Core.Mods;
 using PatchZone.Hatch;
 
@@ -27,8 +28,30 @@ namespace PatchZone.Mods
         {
             this.Assemblies = new List<Assembly>();
 
+            //TODO: Hack, remove when IDataSource is part of mod manifest
+            var currentlyLoadedAssemblies = AppDomain.CurrentDomain.GetAssemblies().Select(x =>
+            {
+                var assemblyLocation = x.Location;
+                try
+                {
+                    return Path.GetFileName(assemblyLocation);
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }).ToHashSet();
+
             foreach (string assemblyPath in Directory.EnumerateFiles(modContext.SourcePath, "*.dll", SearchOption.AllDirectories))
             {
+                var assemblyFile = Path.GetFileName(assemblyPath);
+                if(currentlyLoadedAssemblies.Contains(assemblyFile))
+                {
+                    //Assume all assemblies currenty loaded into app domain
+                    //are part of PatchZone so don't reload them from mod folder
+                    continue;
+                }
+
                 var assembly = Assembly.LoadFile(assemblyPath);
                 this.Assemblies.Add(assembly);
             }
